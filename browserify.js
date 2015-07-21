d7a36d0e6a3a159b9e2b909c90cdd3c1dfb3c369
Pablo Molnar <pablomolnar@gmail.com>
2015-07-20 17:32:14 -0700

    Exclude d3 from bundle

--- browserify.js
+++ browserify.js
@@ -9,6 +9,7 @@ if (!fs.existsSync('./dist')) {
 
 browserify({'debug': true, 'standalone': 'Radial Progress Chart'})
   .require('./index.js', {'entry': true})
+  .exclude('d3')
   .bundle()
   .on('error', function(err) { console.log('Error : ' + err.message); })
   .pipe(fs.createWriteStream('dist/radial-progress-chart.js'));
